f5c714ce697b8da84444a356797ad8e70aec14bb
fibbonnaci <jyothinookula@gmail.com>
2016-07-30 09:05:44 -0700

    Update main.js

--- main.js
+++ main.js
@@ -99,6 +99,22 @@ for(i=1; i<=6; i++){
 // If the number is divisible by 5, print "Buzz" instead of the number.
 // If the number is divisible by both 3 and 5, print "FizzBuzz" instead of the number.
 
+for(i=1; i <=100; i++){
+	if((i%3===0)&&(i%5===0)){
+		console.log("BuzzFizz");
+	}
+	else if(i%5===0){
+		console.log("Buzz");
+	
+	}
+	else if(i%3===0){
+		console.log("Fizz");
+	}
+	else{
+		console.log(i);
+	}
+}
+
 
 
 // RO SHAM BO!
